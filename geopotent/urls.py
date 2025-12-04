21feb8f98a12c15755cd8b20ab97c4fa990a5b04
Kiran Surya Kumar K <kiransuryakumar.k@gmail.com>
2025-12-04 09:27:52 +0530

    updated urls

--- geopotent/urls.py
+++ geopotent/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('potential_app.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('admin/', admin.site.urls),
 ]
